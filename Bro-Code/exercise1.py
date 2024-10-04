21252775df0677fa99b6b74bead78404b325c780
Maaxboon <maaxwelokoth@gmail.com>
2024-10-04 09:22:52 +0300

    Initial commit

---
+++ Bro-Code/exercise1.py
@@ -0,0 +1,8 @@
+# Rectangle Area calculation
+
+length = int(input("Enter the length: "))
+width = int(input("Enter the width: "))
+
+area = length * width
+
+print(f"Area is: {area} m\u00B2 ")
---
+++ Bro-Code/variables.py
@@ -0,0 +1,62 @@
+# Variable = A container for a value (string, integer, float, boolean)
+# A variable behaves as if it was the value it contains
+
+
+# Strings
+first_name = "Maax"
+
+food = "Chapati"
+
+# print(first_name)
+
+email ="maaxboon@mail.com"
+
+user_name = Maaxboon
+
+# print using an f(formatted string)
+# print(f"Hello {first_name}")
+# print(f"You like {food}")
+# print(f"Your email is: {email}")
+
+# Integers
+
+age = 24
+
+quantity = 3
+
+num_of_students = 30
+
+year = 2024
+
+# print(f"You are {age} years old")
+# print(f"You are buying {quantity} items")
+# print(f"Your class has {num_of_students} students")
+
+# Float
+
+price = 10.99
+gpa = 3.2
+distance = 5.5
+pi = 3.14
+
+# print(f"The price is KES{price}")
+# print(f"Your gpa is {gpa}")
+# print(f"You ran {distance} kilometers")
+
+# Booleans
+is_student = False
+
+for_sale = False
+
+is_online = True
+
+is_admin = False
+
+print(f"Are you a student?: {is_student}")
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
+
+    
