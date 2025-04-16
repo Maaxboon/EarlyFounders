671ea3d0a341c1495741a5ae91d098b0616dfe9c
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-16 07:59:18 +0300

    Sample complex if statement

--- 5-control-structures/comparative-operators.py
+++ 5-control-structures/comparative-operators.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
-x = 6
-if x < 1 or x > 5:
-    print("x is less than 1")
-    print("x is greater than 5")
+# x = 6
+# if x < 1 or x > 5:
+#    print("x is less than 1")
+#   print("x is greater than 5")
+
+x = 3
+
+if x == 1:
+    print("x = 1")
+elif x == 2:
+    print("x = 2")
+else:
+    print("The value of x is neither 1 nor 2")
