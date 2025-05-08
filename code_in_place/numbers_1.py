4a261828dadf3eff63c2b0825996e587647ba0ef
Maaxboon <maaxwelokoth@gmail.com>
2025-05-08 17:32:12 +0300

    Removes redundancy

--- code_in_place/numbers_1.py
+++ code_in_place/numbers_1.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 def main():
     print("This program adds two numbers.")
-    num1 = input("Enter first number: ")
-    num1 = int(num1)
-    num2 = input("Enter the second number: ")
-    num2 = int(num2)
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter the second number: "))
     total = num1 + num2
     print("The total is " + str(total) + ".")
 main()
