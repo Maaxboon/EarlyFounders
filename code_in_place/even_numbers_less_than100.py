267d2685808b12a6dacba915c5a78359d2fb54ca
Maaxboon <maaxwelokoth@gmail.com>
2025-05-18 07:08:48 +0300

    Printing even numbers less than 100

---
+++ code_in_place/even_numbers_less_than100.py
@@ -0,0 +1,12 @@
+def main():
+    for i in range(100):
+        # next_even = i * 2
+        # print(next_even)
+        # if i <= 100:
+        #     break
+        if (i % 2 == 0):
+            print(i)
+        else:
+            continue
+if __name__ == "__main__":
+    main()
