de1fc48ccfef580a90a89b8cbaec06ce208cd1f7
Maaxboon <maaxwelokoth@gmail.com>
2025-07-23 17:51:19 +0300

    basic input/output and variable assignment by swapping two numbers

--- module_1_exercises/variable_swap.py
+++ module_1_exercises/variable_swap.py
@@ -4,4 +4,23 @@ Purpose: Swap two user-input numbers
 Author: Maxwel Okoth
 Date: 2025-07-07
 """
-a = input(int("Enter a number"))
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+
+# Swap logic
+temp = a
+a = b
+b = temp
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+
+# Assigning original values 
+temp = b
+b = a
+a = temp
+
+print("After reassigning:")
+print("a =", a)
+print("b =", b)
